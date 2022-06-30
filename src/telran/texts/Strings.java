bdf63d30f0b2f330ed7c208f9b3e03f91cc426fa
diana <dcecurskaya@gmail.com>
2022-06-30 20:49:50 +0300

    CW# 6

---
+++ src/telran/texts/Strings.java
@@ -0,0 +1,20 @@
+package telran.texts;
+
+public class Strings {
+	/**
+	 * 
+	 * @param str1 ascii string with no repeated symbols
+	 * @param str2 ascii string with no repeated symbols
+	 * @return array with two numbers - 
+	 * 1. number of the symbols of the str2 that exist in str2 at the same indexes
+	 * 2. number of the symbols of the str2 that exist in str2 at different indexes
+	 */
+	public static int[] deepNoRepeatedCompare(String str1, String str2) {
+		//TODO
+		/*
+		 * 2 implementations hints
+		 * 1. additional helper array such that array['a'] is an index of the symbol 'a' in string
+		 */
+		return null;
+	}
+}
